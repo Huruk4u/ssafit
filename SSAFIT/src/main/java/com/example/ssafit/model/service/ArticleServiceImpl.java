39bb194f3ff0cb28dea09b9395ebee54c1b759cc
Huruk4u <sungmin91529@gmail.com>
2025-05-08 23:25:18 +0900

    Update ArticleModel : SeongminYoo updated File : ArticleMapper.xml, ArticleService

---
+++ SSAFIT/src/main/java/com/example/ssafit/model/service/ArticleServiceImpl.java
@@ -0,0 +1,66 @@
+package com.example.ssafit.model.service;
+
+import com.example.ssafit.model.dao.ArticleDao;
+import com.example.ssafit.model.dao.ArticleService;
+import com.example.ssafit.model.dto.Article;
+import com.example.ssafit.model.dto.SearchCondition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ArticleServiceImpl implements ArticleService {
+
+    @Autowired
+    private ArticleDao articleDao;
+
+    @Override
+    public List<Article> searchAllArticle() {
+        return articleDao.selectAllArticle();
+    }
+
+    @Override
+    public Article searchArticleByArticleId(int articleId) {
+        return articleDao.selectArticleByArticleId(articleId);
+    }
+
+    @Override
+    public List<Article> searchArticleListByUserId(int userId) {
+        return articleDao.selectArticleListByUserId(userId);
+    }
+
+    @Override
+    public List<Article> searchArticleListByArticleLikeUserId(int userId) {
+        return articleDao.selectArticleListByArticleLikeUserId(userId);
+    }
+
+    @Override
+    public List<Article> searchArticleListByBoardId(int boardId) {
+        return articleDao.selectArticleListByBoardId(boardId);
+    }
+
+    @Override
+    public List<Article> searchArticleListByCondition(SearchCondition condition) {
+        return articleDao.selectArticleListByCondition(condition);
+    }
+
+    @Override
+    @Transactional
+    public int addArticle(Article article) {
+        return articleDao.insertArticle(article);
+    }
+
+    @Override
+    @Transactional
+    public int modifyArticle(int articleId, Article article) {
+        return articleDao.updateArticle(articleId, article);
+    }
+
+    @Override
+    @Transactional
+    public int removeArticle(int articleId) {
+        return articleDao.deleteArticle(articleId);
+    }
+}
