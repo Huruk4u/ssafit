29f215f869c68925dcdad6a56fbfbcacdea90015
Huruk4u <sungmin91529@gmail.com>
2025-05-08 22:37:54 +0900

    Update ArticleDao, SearchCondition : SeongminYoo updated File : ArticleDao, SearchCondition

--- SSAFIT/src/main/java/com/example/ssafit/controller/BoardController.java
+++ SSAFIT/src/main/java/com/example/ssafit/controller/BoardController.java
@@ -2,7 +2,6 @@ package com.example.ssafit.controller;
 
 import com.example.ssafit.exception.BoardNotFoundException;
 import com.example.ssafit.model.dto.Board;
-import com.example.ssafit.model.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
