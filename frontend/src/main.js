aed7431962755f8d0f42bbd498583b983b1bde63
seongminYoo <sungmin91529@gmail.com>
2025-05-22 16:54:49 +0900

    modify mypage

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import api from '@/api/axiosInstance'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/fonts.css';
 
 const token = localStorage.getItem('token')
 if (token) {
